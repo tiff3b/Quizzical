51abf6fba994fea231c0cc338ae1b164c1913479
tiff3b <tiffany3berger@gmail.com>
2026-04-19 09:00:47 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App (){
+
+return(
+  <div className='page'>
+    <section className='intro'>
+      <h1>Quizzical</h1>
+      <h2>Can you beat the quiz?</h2>
+      <button className="btn-start">
+        Start quiz
+      </button>
+    </section>
+  </div>
+)}
